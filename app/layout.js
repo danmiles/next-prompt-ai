2ec0a8a1747e7d44ba7109b3a54b469c84cb9025
Denis <41701228+danmiles@users.noreply.github.com>
2023-05-25 00:19:20 +0200

    all setup is ready

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,28 @@
 import { Inter } from 'next/font/google';
- 
+import '@styles/global.css';
+
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-  variables: 'main-font'
+  variables: 'main-font',
 });
- 
+
+export const metadata = {
+  title: 'Prompt AI',
+  description:
+    'On this site you can create hints on different topics for gpt chat',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
-      <body>{children}</body>
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">{children}</main>
+      </body>
     </html>
   );
 }
