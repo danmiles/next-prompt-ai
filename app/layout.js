59998fb7f22ba312398adc25bcaaf0c476e2db8f
Denis <41701228+danmiles@users.noreply.github.com>
2023-05-24 23:14:24 +0200

    downlod files

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+import { Inter } from 'next/font/google';
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variables: 'main-font'
+});
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
