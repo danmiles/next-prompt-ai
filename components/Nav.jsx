4d24a72a3da266dd376a2d326d45a487dd13bf93
Denis <41701228+danmiles@users.noreply.github.com>
2023-05-29 00:20:06 +0200

    nav component start

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,20 +1,35 @@
 'use client';
 
-import React from 'react'
-import Link  from 'next/link';
+import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, useProviders } from 'next-auth/react'
+import { signIn, signOut, useSession, useProviders } from 'next-auth/react';
 
 function Nav() {
+  const isUserLoggedIn = true;
   return (
-    <nav className='flex-between w-full pt-3 mb-16'>
-      <Link href='/' className='flex gap-2 flex-center'> 
-        <Image src='/assets/images/logo.svg' width={40} height={40} alt='logo' />
+    <nav className="flex-between w-full pt-3 mb-16">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          width={40}
+          height={40}
+          alt="logo"
+        />
         <p className="logo_text second-font">Promt-AI</p>
       </Link>
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-promt" className='black_btn'>Create post</Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
