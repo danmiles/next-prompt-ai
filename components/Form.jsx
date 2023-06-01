facef277a82594a07037ed4b66e8c218dc3aacda
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-02 01:25:09 +0200

    create-prompt page start

--- components/Form.jsx
+++ components/Form.jsx
@@ -4,13 +4,18 @@ import Link from 'next/link';
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full">
-      <div className="">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        aliquam alias, perspiciatis ducimus maxime esse, et tempora similique
-        earum quam reiciendis pariatur voluptatem ut, aspernatur ad doloremque
-        minima quae beatae reprehenderit corporis? Sit tempora magni dolor
-        eligendi et autem maiores?
-      </div>
+      <h1 className="head_text">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+
+      <p className="desc">
+        {type} and share prompts with other users. You can also use this site to
+        create prompts for gpt chat. Good prompts are the backbone of good
+        outputs. This is true for ChatGPT and even more so for AI image
+        generation tools. But while ChatGPT provides solid responses even with
+        super basic prompts, AI art generators tend to require more attention to
+        detail—and more attempts—before you get what you're looking for.
+      </p>
     </section>
   );
 };
