93a39fb815beb2bb63fb13d386da2b90d2990a7e
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-02 22:27:16 +0200

    comm

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
 
 function Feed() {
+  const [searchText, setSearchText] = useState('');
+  const handleSearchChange = (e) => {
+    
+  }
   return (
-    <div>Feed</div>
-  )
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Serch for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          className="search_input peer"
+        />
+      </form>
+    </section>
+  );
 }
 
-export default Feed
+export default Feed;
