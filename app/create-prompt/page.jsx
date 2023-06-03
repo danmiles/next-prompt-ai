3b839fc48479b870ad332d78ee82a9222ebf72ac
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-03 23:15:55 +0200

    add

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,47 +1,45 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import Form from '@components/Form';
+import Form from "@components/Form";
 
-const CreatePromt = () => {
-  const [submitting, setSubmitting] = useState(false);
+const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  });
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
   const createPrompt = async (e) => {
     e.preventDefault();
-    setSubmitting(true);
+    setIsSubmitting(true);
+
     try {
-      const res = await fetch('/api/prompt/new', {
-        method: 'POST',
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
           tag: post.tag,
         }),
       });
-      setSubmitting(false);
 
-      if (res.ok) {
-        router.push('/');
+      if (response.ok) {
+        router.push("/");
       }
     } catch (error) {
-      console.error(error);
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
   return (
     <Form
-      type="Create"
+      type='Create'
       post={post}
       setPost={setPost}
       submitting={submitting}
@@ -50,4 +48,4 @@ const CreatePromt = () => {
   );
 };
 
-export default CreatePromt;
+export default CreatePrompt;
