ee00be7115372aa834fce52cf4414eb0d2532be5
Denis <41701228+danmiles@users.noreply.github.com>
2023-06-08 23:00:26 +0200

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,59 +1,62 @@
-import React from 'react';
-import Link from 'next/link';
+import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <section className="flex flex-col items-center">
-      <h1 className="head_text">
-        <span className="blue_gradient">{type} Post</span>
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span>
       </h1>
-
-      <p className="desc mb-10">
-        {type} and share prompts with other users. You can also use this site to
-        create prompts for gpt chat. Good prompts are the backbone of good
-        outputs. This is true for ChatGPT and even more so for AI image
-        generation tools. But while ChatGPT provides solid responses even with
-        super basic prompts, AI art generators tend to require more attention to
-        detail—and more attempts—before you get what you're looking for.
+      <p className='desc text-left max-w-md'>
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform
       </p>
 
       <form
         onSubmit={handleSubmit}
-        className="w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
       >
-        <label className="second-font font-semibold text-base text-gray-700">
-          Your AI Prompt
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder='Write your post here'
+            required
+            className='form_textarea '
+          />
         </label>
 
-        <textarea
-          value={post.prompt}
-          onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-          placeholder="Write your prompt here..."
-          required
-          className="form_textarea"
-        />
-        <label className="second-font font-semibold text-base text-gray-700">
-          Tag <span>(#webdevelopment, #idea, #product) </span>
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Field of Prompt{" "}
+            <span className='font-normal'>
+              (#product, #webdevelopment, #idea, etc.)
+            </span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            type='text'
+            placeholder='#Tag'
+            required
+            className='form_input'
+          />
         </label>
 
-        <input
-          value={post.tag}
-          onChange={(e) => setPost({ ...post, tag: e.target.value })}
-          placeholder="Write your prompt here..."
-          required
-          className="form_input mb-3"
-        />
-
-        <div className="flex-end mx-3  gap-4">
-          <Link
-            href="/"
-            className="text-gray-500 text-sm text-right transition-all duration-300 hover:text-red-500"
-          >
-            {' '}
+        <div className='flex-end mx-3 mb-5 gap-4'>
+          <Link href='/' className='text-gray-500 text-sm'>
             Cancel
           </Link>
-          <button className="create-btn" type="submit" disabled={submitting}>
-            {submitting ? `...${type}` : type}
+
+          <button
+            type='submit'
+            disabled={submitting}
+            className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'
+          >
+            {submitting ? `${type}ing...` : type}
           </button>
         </div>
       </form>
